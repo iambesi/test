fc0e962e6761611c79bf567e0e1885ae1f4a904d
Bernard Simmons <bjs_3889@yahoo.com>
2021-11-15 13:23:38 -0500

    Added new files for testing

---
+++ ckos.ps1
@@ -0,0 +1,7 @@
+﻿get-computerinfo | select windowsproductname, windowsversion, osname, osarchitecture
+
+get-computerinfo | select osname, windowsversion, osarchitecture
+
+(Get-Item "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion").GetValue('ReleaseID')  ProductName
+
+# Source: https://superuser.com/questions/1330269/how-to-get-os-version-through-powershell-with-wmi
